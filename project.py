db3195c9a9953c2faa4edb7c533f4e50fd538645
MinahilAmer <89538403+MinahilAmer@users.noreply.github.com>
2023-06-20 01:02:45 +0500

    project.py

--- project.py
+++ project.py
@@ -11,20 +11,26 @@ def calculate_standard_deviation(data):
     std_deviation = math.sqrt(variance)
     return std_deviation
 
-def find_minimum(data):
-    min_val = min(data)
-    return min_val
+def test_calculate_mean():
+    # Test case 1
+    data = [1, 2, 3, 4, 5]
+    assert calculate_mean(data) == 3.0
+    
+    # Test case 2
+    data = [0, 0, 0, 0]
+    assert calculate_mean(data) == 0.0
 
-def find_maximum(data):
-    max_val = max(data)
-    return max_val
-
-def sort_data(data):
-    sorted_data = sorted(data)
-    return sorted_data
+def test_calculate_standard_deviation():
+    # Test case 1
+    data = [1, 2, 3, 4, 5]
+    assert calculate_standard_deviation(data) == 1.4142135623730951
+    
+    # Test case 2
+    data = [0, 0, 0, 0]
+    assert calculate_standard_deviation(data) == 0.0
 
 def main():
-    data = [1, 7, 3, 2, 5, 9, 4, 6, 8]
+    data = [1, 2, 3, 4, 5]
     
     # Calculate mean
     mean = calculate_mean(data)
@@ -33,18 +39,8 @@ def main():
     # Calculate standard deviation
     std_deviation = calculate_standard_deviation(data)
     print("Standard Deviation:", std_deviation)
-    
-    # Find the minimum value
-    min_val = find_minimum(data)
-    print("Minimum Value:", min_val)
-    
-    # Find the maximum value
-    max_val = find_maximum(data)
-    print("Maximum Value:", max_val)
-    
-    # Sort the data
-    sorted_data = sort_data(data)
-    print("Sorted Data:", sorted_data)
+
+   
 
 if __name__ == '__main__':
     main()
